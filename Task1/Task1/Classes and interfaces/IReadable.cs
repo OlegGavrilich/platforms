f98e547071ed0b7a6b8aa5c3c2fe5c4ea4a6c528
OlegProgramist <Gavrilich.oleg.lviv@gmail.com>
2019-01-02 16:44:37 +0200

    add interface IReadable

---
+++ Task1/Task1/Classes and interfaces/IReadable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Classes_and_interfaces
+{
+    interface IReadable
+    {
+        void Read(List<Currency> ls);
+
+    }
+}
