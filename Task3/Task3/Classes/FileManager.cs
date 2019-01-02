fecca8db6f04ebb328fa492cdb473dcb3ee272c8
OlegProgramist <Gavrilich.oleg.lviv@gmail.com>
2019-01-02 17:27:49 +0200

    add interface FileManager

---
+++ Task3/Task3/Classes/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Classes
+{
+    public interface FileManager
+    {
+        /// <summary>
+        /// Method witch read file
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Read(string filePath);
+        /// <summary>
+        /// Method witch write to file
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Write(string filePath);
+    }
+}
