2d7183de27620e0a4287cf0dd58335636a3b3cc4
OlegProgramist <Gavrilich.oleg.lviv@gmail.com>
2019-01-02 16:49:35 +0200

    add enum of currencies

---
+++ Task1/Task1/Enum for currencies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public enum CurrencyName
+    {
+
+        /// <summary>
+        /// Invalid currency name
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Currency name for dollars
+        /// </summary>
+        Dollar,
+
+        /// <summary>
+        /// Currency name for euroes
+        /// </summary>  
+        Euro,
+
+        /// <summary>
+        /// Currency name for gryvnyas
+        /// </summary>
+        UAH
+
+    }
+}
