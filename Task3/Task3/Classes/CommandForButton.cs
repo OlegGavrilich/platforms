37487d9c74db426e6d36178963c2da03dc2549cc
OlegProgramist <Gavrilich.oleg.lviv@gmail.com>
2019-01-02 17:30:29 +0200

    add class CommandForButton

---
+++ Task3/Task3/Classes/CommandForButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task3.Classes
+{
+    class CommandForButton
+    {
+        /// <summary>
+        /// Button delete for get value
+        /// </summary>
+        public static RoutedCommand Delete { get; set; }
+        /// <summary>
+        /// Button tolist for get list
+        /// </summary>
+        public static RoutedCommand ToList { get; set; }
+        /// <summary>
+        /// Some method what can with commands
+        /// </summary>
+        static CommandForButton()
+        {
+            Delete = new RoutedCommand("Delete", typeof(MainWindow));
+            ToList = new RoutedCommand("ToList", typeof(MainWindow));
+        }
+    }
+}
