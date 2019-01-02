80667fad1e5d474a806a9a05fa3189188ce0f9da
OlegProgramist <Gavrilich.oleg.lviv@gmail.com>
2019-01-02 16:45:59 +0200

    add Pair of Currency

---
+++ Task1/Task1/Classes and interfaces/PairOfCurrency.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Classes_and_interfaces
+{
+    class PairOfCurrency
+    {
+        public static Dictionary<CurrencyName, double> DoPair(IEnumerable<Currency> List)
+        {
+            Dictionary<CurrencyName, double> currencies = new Dictionary<CurrencyName, double>();
+            foreach (var i in List)
+            {
+                if (currencies.ContainsKey(i.CurrencyName))
+                {
+                    currencies[(i.CurrencyName)] += i.Amount;
+                }
+                else
+                {
+                    currencies.Add(i.CurrencyName, i.Amount);
+                }
+            }
+            if (currencies.Count == 0)
+            {
+                throw new ArgumentNullException("Dictionary is empty!");
+            }
+            return currencies;
+        }
+    }
+}
